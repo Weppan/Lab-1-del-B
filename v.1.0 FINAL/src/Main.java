d2474b8eb9c8e1fca587ff651d5c5f9de546ef59
Weppan <93657080+Weppan@users.noreply.github.com>
2021-11-26 09:53:02 +0100

    Add files via upload

---
+++ v.1.0 FINAL/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public void userInterface(Moveandturn moveable) {
+        char x = ' ';
+        //char x = getCharacterFromUser();
+        if (x == 'm') moveable.move();
+        if (x == 'l') moveable.turnLeft();
+    }
+
+    public static void main (String[] args) {
+        Volvo240 car = new Volvo240();
+//        userInterface(car);
+/*        Saab95 car2 = new Saab95();
+
+        car
+        System.out.println("Number of doors: " + car.getNrDoors());
+        System.out.println("Engine power: " + car.getEnginePower());
+        System.out.println("Current speed: " + car.getCurrentSpeed());
+        car.startEngine();
+        System.out.println("Current speed: " + car.getCurrentSpeed());
+        car.gas(0.5);
+        System.out.println("Current speed: " + car.getCurrentSpeed());*/
+
+        car.startEngine();
+        car.move();
+        car.turnLeft();
+        car.turnLeft();
+        car.turnRight();
+
+    }
+}
