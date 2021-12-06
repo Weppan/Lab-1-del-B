0f75882b34cc26526112389971215e15946695b1
egordya <91674367+egordya@users.noreply.github.com>
2021-12-06 11:05:18 +0100

    Fixing right version

--- v.1.0/src/Main.java
+++ v.1.0/src/Main.java
@@ -61,10 +61,6 @@ public class Main {
 
 
 
-        repairshop1.receiveCar(car);
-        repairshop1.receiveCar(car2);
-        repairshop1.retrieveCar(car);
-
 
 
 //        String word1 = new String("Hello");
